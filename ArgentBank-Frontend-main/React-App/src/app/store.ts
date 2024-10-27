7cce1a3ad047d03350f0ef322d1509e49ea3d1bc
Yass <ynezreg@gmail.com>
2024-10-27 16:46:11 +0100

    chore(store): configure Redux store with auth reducer and development state access

---
+++ ArgentBank-Frontend-main/React-App/src/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../app/feature/authSlice';
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+//Pour rendre le store accessible dans la console du navigateur mais pas en mode production
+//utiliser store.getState() pour obtenir l'état actuel du store dans la console du navigateur
+if (process.env.NODE_ENV !== 'production') {
+    (window as any).store = store;
+}
+
+//Exportation du store
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
