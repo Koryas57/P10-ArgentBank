52c5dfb4ef76b0f28bdebb7b35846ba019cf85c5
Yass <ynezreg@gmail.com>
2024-10-28 13:28:24 +0100

    style: apply minor UI adjustments and styling tweaks

--- ArgentBank-Frontend-main/React-App/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ ArgentBank-Frontend-main/React-App/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -7,6 +7,8 @@ import { RootState } from '../../app/store';
 //Composant pour les routes protégées , ici la route protégée est celle du profil
  export const ProtectedRoute: React.FC = () => {
     const { isAuthenticated} = useSelector((state: RootState) => state.auth);
+
+    
   // Si l'utilisateur n'est pas authentifié, on le redirige vers la page de connexion
   // Si l'utilisateur est authentifié on affiche les composants enfants de la route protégée
   // Le composant Outlet de react-router-dom permet d'afficher les composants enfants de la route protégée
